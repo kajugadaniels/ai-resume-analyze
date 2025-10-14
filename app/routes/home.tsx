a58a33713d022b8c072213039dd6827ac2bad697
unknown <kajugadaniels@gmail.com>
2025-10-14 21:15:42 +0200

    Setup project

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Rusumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
